b405b33e58c8ed699c160cbe6b3a4661c9714f65
ribhavj <ribhavjain9@gmail.com>
2022-02-13 15:44:49 +0530

    Added Analytics

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import TagManager from "react-gtm-module";
+
 import "./App.css";
 import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
@@ -5,6 +7,11 @@ import { chosenTheme } from "./theme";
 import { GlobalStyles } from "./global";
 
 function App() {
+  const tagManagerArgs = {
+    gtmId: process.env.REACT_APP_GTM_TRACKING_ID,
+  };
+  TagManager.initialize(tagManagerArgs);
+
   return (
     <ThemeProvider theme={chosenTheme}>
       <>
